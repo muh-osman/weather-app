27a4e7513084bb915835f5adce4b3cf0f5b8c4d8
Muhammed Osman <91621437+Muh-Osman@users.noreply.github.com>
2023-02-13 12:28:36 +0300

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Getting Started with Create React App
+# Credit: <https://youtu.be/Reny0cTTv24>
+
+## Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "8cbc2a055amsh0a6844c99a5b1c4p158252jsnf13650be72b1",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"; // From https://openweathermap.org/current
+export const WEATHER_API_KEY = "8ce606967076b60262088310bddc6c85"; // From https://home.openweathermap.org/api_keys
